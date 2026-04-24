fa026e686433e77d1b7aee54ad9e3d1ec21878c8
arty1223 <65350351+arty1223@users.noreply.github.com>
2026-04-25 00:11:02 +0300

    manufacturers dict

---
+++ app.py
@@ -0,0 +1,16 @@
+import json
+with open ("manufacturers.txt", "r") as f:
+    table = f.readlines()
+K = -1
+manufacturers = {}
+for i in table:
+    if i == "COMPANY 8 7 6 5 4 3 2 1 HEX\n":
+        K+=1
+        continue
+    elif i == "Continuation Code 0 1 1 1 1 1 1 1 7F\n":
+        continue
+    t = ' '.join(i[:-19].split()[1:])
+    manufacturers[hex(K)[2:].capitalize() + i[-3:-1]] = t
+
+with open("./manufacturers.json", "w") as fp:
+        json.dump(manufacturers , fp, ensure_ascii=False) 
